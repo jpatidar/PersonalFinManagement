444d85d7ebb941b25defba2512eda2f838f2b73f
jpatidar <jpatidar@gmail.com>
2017-11-29 18:45:06 +0530

    testing commit from eclipse

--- src/com/personal/finance/management/impl/TestSecond.java
+++ src/com/personal/finance/management/impl/TestSecond.java
@@ -3,7 +3,8 @@ package com.personal.finance.management.impl;
 public class TestSecond {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Testing commit from eclipse");
 
 	}
 
